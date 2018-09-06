e5d9c5d18df15e674e0d8872fad3a7c7e2df75b1
Jamee Kim <jameekim@nodaplife.me>
2018-09-06 10:25:22 +0900

    add typescript definitions (#27)

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+/**
+ * A wrapper class to promisify web workers
+ */
+declare class PromiseWorker {
+  /**
+   * Pass in the worker instance to promisify
+   *
+   * @param worker The worker instance to wrap
+   */
+  constructor(worker: Worker);
+
+  /**
+   * Send a message to the worker
+   *
+   * The message you send can be any object, array, string, number, etc.
+   * Note that the message will be `JSON.stringify`d, so you can't send functions, `Date`s, custom classes, etc.
+   *
+   * @param userMessage Data or message to send to the worker
+   * @returns Promise resolved with the processed result or rejected with an error
+   */
+  public postMessage<TResult = any, TInput = any>(userMessage: TInput): Promise<TResult>;
+}
+
+export = PromiseWorker;
---
+++ register.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Make this worker a promise-worker
+ *
+ * @param callback Callback function for processing the inbound data
+ */
+declare function registerPromiseWorker<TMessageIn = any, TMessageOut = any>(
+  callback: (message: TMessageIn) => Promise<TMessageOut> | TMessageOut
+): void;
+
+export = registerPromiseWorker;
